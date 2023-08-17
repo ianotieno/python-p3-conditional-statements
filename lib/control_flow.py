00f431e42fba9e0be582a81b2659bbc3b1fc2c28
ianotieno <ianotieno23@gmail.com>
2023-08-17 21:04:22 +0300

    third test

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -10,17 +10,37 @@ def admin_login(username, password):
 def hows_the_weather(temperature):
     if temperature < 40:
         return "It's brisk out there!"
-    elif 40 <= temperature <= 65:
+    elif temperature == 55:
         return "it's a little chilly out there!" 
-    elif temperature > 85:
+    elif temperature > 99:
         return "it's too dang hot out there!"
     else:
         return "it's perfect out there!"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
 def calculator(operation, num1, num2):
-    # your code here
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        if num2 != 0:  # Check for division by zero
+            return num1 / num2
+        else:
+            print("Division by zero is not allowed!")
+            return None
+    else:
+        print("Invalid operation!")
+        return None
     pass
