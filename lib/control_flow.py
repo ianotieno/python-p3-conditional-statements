9e134c5ed3fe96af8ad65c02a6e75e6709aeedff
ianotieno <ianotieno23@gmail.com>
2023-08-17 20:49:12 +0300

    first test

---
+++ lib/control_flow.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def admin_login(username, password):
+    if (username.lower() == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+
+
+def hows_the_weather(temperature):
+    # your code here
+    pass
+
+def fizzbuzz(num):
+    # your code here
+    pass
+
+def calculator(operation, num1, num2):
+    # your code here
+    pass
