551bf3ad2834832b6e77a933b15737684f92d643
ianotieno <ianotieno23@gmail.com>
2023-08-17 21:07:00 +0300

    finall push

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -10,12 +10,13 @@ def admin_login(username, password):
 def hows_the_weather(temperature):
     if temperature < 40:
         return "It's brisk out there!"
-    elif temperature == 55:
-        return "it's a little chilly out there!" 
-    elif temperature > 99:
-        return "it's too dang hot out there!"
+    elif 40 <= temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
     else:
-        return "it's perfect out there!"
+        return "It's perfect out there!"
+
 
 def fizzbuzz(num):
     if num % 3 == 0 and num % 5 == 0:
